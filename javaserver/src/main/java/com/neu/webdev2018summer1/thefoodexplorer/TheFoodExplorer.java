cbcca88da2179cda041e1244761653bc082192bf
Neha <shukla.ne@husky.neu.edu>
2018-06-08 16:32:14 -0400

    added foodexplorer main class

---
+++ javaserver/src/main/java/com/neu/webdev2018summer1/thefoodexplorer/TheFoodExplorer.java
@@ -0,0 +1,11 @@
+package com.neu.webdev2018summer1.thefoodexplorer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TheFoodExplorer {
+	public static void main(String[] args) {
+		SpringApplication.run(TheFoodExplorer.class, args);
+	}
+}
